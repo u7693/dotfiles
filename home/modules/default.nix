c03817eec009adaea8ea78f4a150fef005940136
Kousuke Takaki <u7693@protonmail.com>
2021-01-13 22:48:01 +0900

    [home/modules] Create nodejs

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./nodejs.nix
     ./rust.nix
   ];
 }
---
+++ home/modules/nodejs.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.programs.nodejs;
+in {
+  options = {
+    programs.nodejs = {
+      enable = mkEnableOption "NodeJS";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.nodejs;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    home.sessionVariables = {
+      NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
+    };
+
+    home.sessionPath = [ "${config.xdg.dataHome}/npm/bin" ];
+
+    # TODO: tmp=${XDG_RUNTIME_DIR}/npm
+    xdg.configFile."npm/npmrc".text = ''
+      prefix=${config.xdg.dataHome}/npm
+      cache=${config.xdg.cacheHome}/npm
+      init-module=${config.xdg.configHome}/npm/config/npm-init.js
+    '';
+  };
+}
