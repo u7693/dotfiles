b94695dea886b07e61379d83cc22838565e61831
Kousuke Takaki <u7693@protonmail.com>
2021-01-19 19:27:52 +0900

    [home/common] Update git

--- home/common/git.nix
+++ home/common/git.nix
@@ -9,15 +9,21 @@ let
     st = "status --short --branch";
   };
 
+  extraConfig = {
+    init.defaultBranch = "main";
+  };
+
   ignores = import ./git-ignores.nix;
 in
 {
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.git.enable
   programs.git = {
     enable = true;
-    inherit aliases ignores;
+    inherit aliases extraConfig ignores;
     signing.key = user.gpg;
     userEmail = user.email;
     userName = user.name;
   };
+
+  home.packages = [ pkgs.gitAndTools.gitflow ];
 }
