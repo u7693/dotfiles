9d399bce98b2b6784915bb633bd43517afdf730f
Kousuke Takaki <u7693@protonmail.com>
2021-01-26 06:32:44 +0900

    [nixos] Use OS prober

--- distro/hydrogen/configuration.nix
+++ distro/hydrogen/configuration.nix
@@ -17,15 +17,6 @@
     gnome3.enable = true;
   };
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
--- nixos/default.nix
+++ nixos/default.nix
@@ -3,4 +3,9 @@
     ./gnome3.nix
     ./user.nix
   ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 }
