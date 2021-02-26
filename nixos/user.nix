ff0fc9810bee7469017ea1fe21bb028514fcfa4a
Kousuke Takaki <u7693@protonmail.com>
2021-02-26 18:05:34 +0900

    [nixos] Improve user.nix

--- nixos/user.nix
+++ nixos/user.nix
@@ -1,24 +1,30 @@
 { config, lib, pkgs, ... }:
 
 with lib; with builtins;
-let
-  cfg = config.u7693.nixos.user;
-
-  user = fromJSON (readFile ../user.json);
-
+let cfg = config.u7693.nixos.user;
 in {
   options = {
     u7693.nixos.user = {
       enable = mkEnableOption "user";
     };
+
+    userName = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+    };
+
+    description = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+    };
   };
 
   config = mkIf cfg.enable {
     users.users.u7693 = {
-      home = "/home/" + user.username;
-      name = user.username;
+      home = "/home/" + cfg.userName;
+      name = cfg.userName;
       createHome = true;
-      description = user.name;
+      description = cfg.description;
       isNormalUser = true;
       extraGroups = [ "wheel" ];
     };
